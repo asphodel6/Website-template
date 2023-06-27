75ccd04fa8e3f692026953d00676f7d95eb5f95f
asphodel6 <harmsss@mail.ru>
2023-06-28 01:38:35 +0500

    Adaptive header

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from '@taiga-ui/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TuiInputModule } from '@taiga-ui/kit';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    FormsModule,
+    TuiInputModule
+  ],
+  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
